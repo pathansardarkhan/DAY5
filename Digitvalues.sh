643b7f983dd7da091acdfcb106a55ddefeac3dcb
pathansardarkhan <pathan.sardarkhan6@gmail.com>
2022-04-09 14:16:38 +0530

    Added: Random

---
+++ Digitvalues.sh
@@ -0,0 +1,9 @@
+R1=$((RANDOM%90 +10))
+R2=$((RANDOM%90 +10))
+R3=$((RANDOM%90 +10))
+R4=$((RANDOM%90 +10))
+R5=$((RANDOM%90 +10))
+sum=$((R1+R2+R3+R4+R5+R6))
+Avg=$((sum/5))
+echo=$sum;
+echo=$avg;	
